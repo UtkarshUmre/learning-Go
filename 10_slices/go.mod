b4e334b14f2776803fcd67d473e4ed16b9f7b3d0
utkarsh <utkarshumre@outlook.com>
2024-01-03 23:06:06 +0530

    slices in go

---
+++ 10_slices/go.mod
@@ -0,0 +1,3 @@
+module slicesinGO
+
+go 1.21.4
---
+++ 10_slices/main.go
@@ -0,0 +1,15 @@
+package main
+
+import "fmt"
+
+func main() {
+
+	fmt.Println("slices in go lang")
+
+	//initialization and declaration of slices
+	var fruitList = []string{"tomato", "apple", "mango", "guava", "banana", "strawberry", "kiwi", "papaya"}
+	fmt.Println(fruitList)
+	fmt.Println("fruitlist length is", len(fruitList))
+	fmt.Printf("fruitlist is of type %T\n", fruitList)
+
+}
