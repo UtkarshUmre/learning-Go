95519ca67e6bca46c2fc2e497d38c3eb69271ce6
utkarsh <utkarshumre@outlook.com>
2024-01-16 23:45:40 +0530

    modifired pointers main.go

--- 08_pointers/main.go
+++ 08_pointers/main.go
@@ -3,5 +3,18 @@ package main
 import "fmt"
 
 func main() {
-	fmt.Println("pointers in go lang")
+	// fmt.Println("pointers in go lang")
+
+	// 	var ptr *int
+	// 	fmt.Println(ptr)
+	// 	fmt.Printf("type of ptr is: %T\n", ptr)
+
+	myNumber := 45
+	var ptr = &myNumber
+	fmt.Println("value of actual pointer is ", ptr)
+	fmt.Printf("type of ptr is: %T\n", ptr)
+
+	*ptr = *ptr + 2
+	fmt.Println(*ptr)
+
 }
