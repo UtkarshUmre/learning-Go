fe8d14cf01a5f7b1623667b8ff115b5e763ef178
utkarsh <utkarshumre@outlook.com>
2024-01-19 20:25:15 +0530

    another one

--- 19_files/main.go
+++ 19_files/main.go
@@ -12,16 +12,26 @@ func main() {
 	content := "the content written in this string needs to go in the file in order to learn file handling in go language"
 	file, err := os.Create("./sampletxtfile.txt")
 
-	if err != nil {
-		panic(err)
-	}
+	checkNilErr(err)
 
 	length, err := io.WriteString(file, content)
+	checkNilErr(err)
+
+	fmt.Println("length is: ", length)
+	defer file.Close()
+	readFile("./sampletxtfile.txt")
+
+}
+
+func readFile(filename string) {
+	databyte, err := os.ReadFile(filename)
+	checkNilErr(err)
+	fmt.Println("text data inside the file is \n", string(databyte))
+
+}
+
+func checkNilErr(err error) {
 	if err != nil {
 		panic(err)
 	}
-
-	fmt.Println("length is: ", length)
-	file.Close()
-
 }
