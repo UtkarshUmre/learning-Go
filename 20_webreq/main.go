0eaf917848457ea2ee7bd978cf48acf1fe2b2e5f
utkarsh <utkarshumre@outlook.com>
2024-01-22 21:41:35 +0530

    webreq learnt

--- 20_webreq/main.go
+++ 20_webreq/main.go
@@ -2,31 +2,22 @@ package main
 
 import (
 	"fmt"
-	"io/ioutil"
 	"net/http"
 )
 
 const url = "https://lco.dev"
 
 func main() {
-	fmt.Println("LCO web request")
+
+	fmt.Println("webrequest in go lang")
 
 	response, err := http.Get(url)
 
 	if err != nil {
 		panic(err)
+
 	}
 
-	fmt.Printf("Response is of type: %T\n", response)
-
-	defer response.Body.Close() // caller's responsibility to close the connection
-
-	databytes, err := ioutil.ReadAll(response.Body)
-
-	if err != nil {
-		panic(err)
-	}
-	content := string(databytes)
-	fmt.Println(content)
+	fmt.Printf("type of response is %T\n", response)
 
 }
