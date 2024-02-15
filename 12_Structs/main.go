469ce85406c4d2da391b349b50dd82aa35388114
utkarsh <utkarshumre@outlook.com>
2024-02-15 23:05:11 +0530

    struc

--- 12_Structs/main.go
+++ 12_Structs/main.go
@@ -3,18 +3,16 @@ package main
 import "fmt"
 
 func main() {
-	fmt.Println("welcome to structs in go lang")
-	// no inheretance in go lang;no super or parent no such concepts in go lang
 
-	utkarsh := User{"utkarsh", "utkarsh@mlh.io", true, 21}
-	fmt.Println(utkarsh)
-	fmt.Printf("utkarsh details are %+v\n", utkarsh)
-	fmt.Printf("name is %v and email is %v\n", utkarsh.Name, utkarsh.Email)
+	fmt.Println("struct in go")
+	Nathan := User{"Nathan", "Nathan@gmail", 23, true}
+	fmt.Println(Nathan)
+
 }
 
 type User struct {
 	Name   string
 	Email  string
-	Status bool
 	Age    int
+	Status bool
 }
