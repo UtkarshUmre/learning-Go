910e35aa01623d1930311f4e5908b797ba513795
utkarsh <utkarshumre@outlook.com>
2024-02-15 23:24:18 +0530

    condtionlas

--- 13_conditionals/main.go
+++ 13_conditionals/main.go
@@ -3,35 +3,16 @@ package main
 import "fmt"
 
 func main() {
-	fmt.Println("If else in golang")
+	fmt.Println("conditionals in go")
 
-	loginCount := 10
+	loginCount := 25
 	var result string
 
 	if loginCount < 10 {
-		result = "Regular user"
-	} else if loginCount > 10 {
-		result = "Watch out"
+		result = "regular user "
 	} else {
-		result = "Exactly 10 login count"
+		result = "not regular user"
 	}
 
 	fmt.Println(result)
-
-	if 9%2 == 0 {
-		fmt.Println("Number is even")
-	} else {
-		fmt.Println("Number is odd")
-	}
-
-	if num := 3; num < 10 {
-		fmt.Println("Num is less than 10")
-	} else {
-		fmt.Println("Num is NOT less than 10")
-	}
-
-	// if err != nil {
-
-	// }
-
 }
