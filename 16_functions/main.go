f3e81c562e459c194b1676638f6bf0bcc212e940
utkarsh <utkarshumre@outlook.com>
2024-02-17 23:45:00 +0530

    fn

--- 16_functions/main.go
+++ 16_functions/main.go
@@ -3,33 +3,19 @@ package main
 import "fmt"
 
 func main() {
-	fmt.Println("Welcome to functions in golang")
-	greeter()
+	fmt.Println("function in go lang")
 
-	result := adder(3, 5)
-	fmt.Println("Result is: ", result)
+	greet() //calling the function
 
-	proRes, myMessage := proAdder(2, 5, 8, 7, 3)
-	fmt.Println("Pro result is: ", proRes)
-	fmt.Println("Pro Message is: ", myMessage)
-
+	result := add(9, 1)
+	fmt.Println(result)
 }
 
-func adder(valOne int, valTwo int) int {
-	return valOne + valTwo
+func greet() { //initializing the funcion in go programming language
+	fmt.Println("namaste from go lang")
 }
 
-func proAdder(values ...int) (int, string) {
-	total := 0
+func add(x int, y int) int {
+	return x + y
 
-	for _, val := range values {
-		total += val
-	}
-
-	return total, "Hi Pro result function"
-
-}
-
-func greeter() {
-	fmt.Println("Namstey from golang")
 }
