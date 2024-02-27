c3d3e4a58414e3fe8044f57ae8e4efbbd063f61b
UtkarshUmre <112888849+UtkarshUmre@users.noreply.github.com>
2024-02-27 17:46:35 +0000

    methods in go
    Signed-off-by: UtkarshUmre <112888849+UtkarshUmre@users.noreply.github.com>

--- 17_methods/main.go
+++ 17_methods/main.go
@@ -2,32 +2,27 @@ package main
 
 import "fmt"
 
-func main() {
-	fmt.Println("Structs in golang")
-	// no inheritance in golang; No super or parent
+// func main(){
+// 	fmt.Println("methods in golang")
+// }
 
-	hitesh := User{"Hitesh", "hitesh@go.dev", true, 16}
-	fmt.Println(hitesh)
-	fmt.Printf("hitesh details are: %+v\n", hitesh)
-	fmt.Printf("Name is %v and email is %v.\n", hitesh.Name, hitesh.Email)
-	hitesh.GetStatus()
-	hitesh.NewMail()
-	fmt.Printf("Name is %v and email is %v.\n", hitesh.Name, hitesh.Email)
+func main() {
+
+	fmt.Println("struct in go")
+	Nathan := User{"Nathan", "Nathan@gmail", 23, true}
+	fmt.Printf("nathans details are %+v\n", Nathan)
+	fmt.Printf("name is %v and emai is %v \n", Nathan.Name, Nathan.Email)
+	Nathan.GetStatus()
 
 }
 
 type User struct {
 	Name   string
 	Email  string
-	Status bool
 	Age    int
+	Status bool
 }
 
-func (u User) GetStatus() {
-	fmt.Println("Is user active: ", u.Status)
-}
-
-func (u User) NewMail() {
-	u.Email = "test@go.dev"
-	fmt.Println("Email of this user is: ", u.Email)
+func (u User) GetStatus(){
+	fmt.Println("is user active ", u.Status)
 }
